575779ed1f747809c44464a815c4fbbd72c16bf8
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-05-30 06:21:43 +0800

    add center menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,73 @@
+import { Search } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   height: 60px;
-  background-color: black;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14;
+  cursor: pointer;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Logo = styled.h1`
+  font-weigth: bold;
 `;
 
 const Navbar = () => {
-  return <Container>Navbar</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Logo>PenguinCoding</Logo>
+        <Center>
+          <Right>Rigth</Right>
+        </Center>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
