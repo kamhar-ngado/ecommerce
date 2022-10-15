cb44e5afe5bbe4e61695ac71e1bc257d58d16212
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-10-16 00:23:32 +0800

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-import Product from "./Pages/Product"
+import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/ProductList" element={<ProductList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
